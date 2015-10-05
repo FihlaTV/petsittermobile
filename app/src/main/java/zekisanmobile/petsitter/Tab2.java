1b2ce7fe6134513459eb2d3c0c56a14e7be055fb
Ezequiel <zeki-san@hotmail.com>
2015-10-04 23:52:38 -0300

    Mais correçoes

--- app/src/main/java/zekisanmobile/petsitter/Tab2.java
+++ app/src/main/java/zekisanmobile/petsitter/Tab2.java
@@ -1,7 +1,11 @@
 package zekisanmobile.petsitter;
 
+import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -11,11 +15,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
@@ -34,14 +44,21 @@ import zekisanmobile.petsitter.Model.Sitter;
 /**
  * Created by ezequiel on 28/09/15.
  */
-public class Tab2 extends Fragment {
+public class Tab2 extends Fragment implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleMap.OnInfoWindowClickListener,
+        GoogleMap.OnMarkerClickListener,
+        LocationListener {
 
     private static final String API_SEARCH_URL = "https://petsitterapi.herokuapp.com/api/v1/sitters";
     private static final String TAG = Tab2.class.getSimpleName();
 
-    // Coordinates used for centering the map
-    private static final double CAMERA_LNG = -30.034647;
-    private static final double CAMERA_LAT = -51.217658;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mCurrentLocation;
+
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected Context context;
 
     // The Map object
     private GoogleMap mMap;
@@ -49,8 +66,89 @@ public class Tab2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.tab_2,container,false);
+
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
         new JSONResponseHandler().execute(API_SEARCH_URL);
+
         return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mGoogleApiClient = new GoogleApiClient.Builder( getActivity() )
+                .addConnectionCallbacks( this )
+                .addOnConnectionFailedListener( this )
+                .addApi( LocationServices.API )
+                .build();
+
+    }
+
+    private void initListeners() {
+        mMap.setOnMarkerClickListener(Tab2.this);
+        mMap.setOnInfoWindowClickListener(Tab2.this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if( mGoogleApiClient != null && mGoogleApiClient.isConnected() ) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 
     private class JSONResponseHandler extends AsyncTask<String, Void, ArrayList<Sitter>> {
@@ -92,6 +190,7 @@ public class Tab2 extends Fragment {
             mMap = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map)).getMap();
 
             if (mMap != null){
+                initListeners();
                 Geocoder gc = new Geocoder(getActivity());
                 try{
                     for(int i = 0; i < receivedSitters.size(); i++) {
@@ -99,16 +198,72 @@ public class Tab2 extends Fragment {
                         mMap.addMarker(new MarkerOptions()
                                 .position(new LatLng(addresses.get(0).getLatitude(), addresses.get(0).getLongitude()))
                                 .title(receivedSitters.get(i).getName())
-                                .icon(BitmapDescriptorFactory.defaultMarker((float) (120 * (8.5 - 6))))
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.pet_marker))
                                 .snippet(receivedSitters.get(i).getAddress()));
                     }
                 }catch (IOException e){
                     Log.d(TAG, e.getMessage());
                 }
+                mCurrentLocation = bestLastKnownLocation(500.0f, (60000 * 5));
+                initCamera( mCurrentLocation );
             }
+        }
 
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(
-                    CAMERA_LAT, CAMERA_LNG)));
+    }
+
+    private void initCamera(Location location) {
+        CameraPosition position = CameraPosition.builder()
+                .target( new LatLng( location.getLatitude(),
+                        location.getLongitude() ) )
+                .zoom( 13f )
+                .bearing(0.0f)
+                .tilt(0.0f)
+                .build();
+
+        mMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(position), null);
+
+        mMap.getUiSettings().setZoomControlsEnabled( true );
+    }
+
+    // Get the last known location from all providers
+    // return best reading that is as accurate as minAccuracy and
+    // was taken no longer then minAge milliseconds ago. If none,
+    // return null.
+
+    private Location bestLastKnownLocation(float minAccuracy, long maxAge) {
+
+        Location bestResult = null;
+        float bestAccuracy = Float.MAX_VALUE;
+        long bestAge = Long.MIN_VALUE;
+
+        List<String> matchingProviders = locationManager.getAllProviders();
+
+        for (String provider : matchingProviders) {
+
+            Location location = locationManager.getLastKnownLocation(provider);
+
+            if (location != null) {
+
+                float accuracy = location.getAccuracy();
+                long time = location.getTime();
+
+                if (accuracy < bestAccuracy) {
+
+                    bestResult = location;
+                    bestAccuracy = accuracy;
+                    bestAge = time;
+
+                }
+            }
+        }
+
+        // Return best reading or null
+        if (bestAccuracy > minAccuracy
+                || (System.currentTimeMillis() - bestAge) > maxAge) {
+            return null;
+        } else {
+            return bestResult;
         }
     }
 }
