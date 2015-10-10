01422ab9ccccc340a225dd9254aad32e7f77b72b
Ezequiel <zeki-san@hotmail.com>
2015-10-10 19:32:59 -0300

    Navigation Drawer

--- app/src/main/java/zekisanmobile/petsitter/DonoHomeActivity.java
+++ app/src/main/java/zekisanmobile/petsitter/DonoHomeActivity.java
@@ -2,16 +2,30 @@ package zekisanmobile.petsitter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import zekisanmobile.petsitter.Adapters.TabsAdapter;
 import zekisanmobile.petsitter.Extras.SlidingTabLayout;
+import zekisanmobile.petsitter.Model.Dono;
 import zekisanmobile.petsitter.Model.Sitter;
 
 public class DonoHomeActivity extends AppCompatActivity {
@@ -19,6 +33,8 @@ public class DonoHomeActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private ViewPager mViewPager;
     private SlidingTabLayout mSlidingTabLayout;
+    private Drawer.Result navigationDrawerLeft;
+    private AccountHeader.Result headerNavigationLeft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +43,20 @@ public class DonoHomeActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.tb_dono);
         toolbar.setTitle("Ezequiel Guilherme");
-        toolbar.setLogo(R.drawable.me_small);
         setSupportActionBar(toolbar);
 
-        // Assigning ViewPager View and setting the adapter
+        // TABS
         mViewPager = (ViewPager) findViewById(R.id.vp_tabs);
         mViewPager.setAdapter(new TabsAdapter(getSupportFragmentManager(), this));
 
-        // Assiging the Sliding Tab Layout View
         mSlidingTabLayout = (SlidingTabLayout) findViewById(R.id.stl_tabs);
         mSlidingTabLayout.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
         mSlidingTabLayout.setBackgroundColor(getResources().getColor(R.color.ColorPrimary));
         mSlidingTabLayout.setSelectedIndicatorColors(getResources().getColor(R.color.ColorAccent));
         mSlidingTabLayout.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
 
             @Override
             public void onPageSelected(int position) {
@@ -49,11 +64,69 @@ public class DonoHomeActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onPageScrollStateChanged(int state) {}
+            public void onPageScrollStateChanged(int state) {
+            }
         });
 
-        // Setting the ViewPager For the SlidingTabsLayout
         mSlidingTabLayout.setViewPager(mViewPager);
+
+        // NAVIGATION DRAWER
+
+        headerNavigationLeft = new AccountHeader()
+                .withActivity(this)
+                .withCompactStyle(false)
+                .withSavedInstance(savedInstanceState)
+                .withThreeSmallProfileImages(false)
+                .withHeaderBackground(R.color.accent_material_light)
+                .withTextColor(R.color.primary_text)
+                .addProfiles(
+                        new ProfileDrawerItem()
+                                .withName("Ezequiel Guilherme")
+                                .withEmail("zeki-san@hotmail.com")
+                                .withIcon(getResources().getDrawable(R.drawable.me))
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener(){
+
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean current) {
+                        Toast.makeText(DonoHomeActivity.this, "profilechanged", Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                })
+                .build();
+
+        navigationDrawerLeft = new Drawer()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withDisplayBelowToolbar(false)
+                .withActionBarDrawerToggleAnimated(true)
+                .withDrawerGravity(Gravity.LEFT)
+                .withSavedInstance(savedInstanceState)
+                .withSelectedItem(0)
+                .withAccountHeader(headerNavigationLeft)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
+
+                    }
+                })
+                .withOnDrawerItemLongClickListener(new Drawer.OnDrawerItemLongClickListener(){
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
+                        Toast.makeText(DonoHomeActivity.this, "OnItemLongClick: " + position, Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                })
+
+                .build();
+
+        navigationDrawerLeft.addItem(new PrimaryDrawerItem()
+                .withName("Pet Sitters")
+                .withIcon(R.drawable.account_star_variant));
+        navigationDrawerLeft.addItem(new PrimaryDrawerItem()
+                .withName("Pet Sitters 2")
+                .withIcon(R.drawable.spotify));
+        navigationDrawerLeft.addItem(new SectionDrawerItem().withName("Configuraçoes"));
     }
 
     @Override
