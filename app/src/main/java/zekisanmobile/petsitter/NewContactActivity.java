c59629d2804784f7eb1f65ea40ddb21cfcc6d57d
Ezequiel <ezequiel.oliveira@iwaysoft.com.br>
2016-02-23 10:06:50 -0300

    Implementing datetimepicker.

--- app/src/main/java/zekisanmobile/petsitter/NewContactActivity.java
+++ app/src/main/java/zekisanmobile/petsitter/NewContactActivity.java
@@ -1,16 +1,32 @@
 package zekisanmobile.petsitter;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
+
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
 
 import zekisanmobile.petsitter.Model.Sitter;
 
-public class NewContactActivity extends AppCompatActivity {
+public class NewContactActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener,
+        TimePickerDialog.OnTimeSetListener, DialogInterface.OnCancelListener{
 
     private Sitter sitter;
+    private TextView tv_datetime;
+    private Button bt_datetime;
+    private int year, month, day, hour, minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +45,110 @@ public class NewContactActivity extends AppCompatActivity {
 
         TextView tv_name = (TextView) findViewById(R.id.tv_name);
         tv_name.setText(sitter.getName());
+
+        tv_datetime = (TextView) findViewById(R.id.tv_datetime);
+        bt_datetime = (Button) findViewById(R.id.bt_datetime);
+        bt_datetime.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                scheduleJob(view);
+            }
+        });
+    }
+
+    private void scheduleJob(View view) {
+        initDateTimeData();
+        Calendar calendarDefault = Calendar.getInstance();
+        calendarDefault.set(year, month, day);
+        DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(
+                this,
+                calendarDefault.get(Calendar.YEAR),
+                calendarDefault.get(Calendar.MONTH),
+                calendarDefault.get(Calendar.DAY_OF_MONTH)
+        );
+
+        Calendar cMin = Calendar.getInstance();
+        Calendar cMax = Calendar.getInstance();
+
+        cMax.set(cMax.get(Calendar.YEAR), 11, 31);
+        datePickerDialog.setMinDate(cMin);
+        datePickerDialog.setMaxDate(cMax);
+
+        List<Calendar> daysList = new LinkedList<>();
+        Calendar[] daysArray;
+        Calendar cAux = Calendar.getInstance();
+        while (cAux.getTimeInMillis() <= cMax.getTimeInMillis()){
+            if(cAux.get(Calendar.DAY_OF_WEEK) != 1 && cAux.get(Calendar.DAY_OF_WEEK) != 7){
+                Calendar c = Calendar.getInstance();
+                c.setTimeInMillis(cAux.getTimeInMillis());
+
+                daysList.add(c);
+            }
+            cAux.setTimeInMillis(cAux.getTimeInMillis() + (24 * 60 * 60 * 1000));
+        }
+
+        daysArray = new Calendar[daysList.size()];
+        for(int i = 0; i < daysArray.length; i++){
+            daysArray[i] = daysList.get(i);
+        }
+
+        datePickerDialog.setSelectableDays(daysArray);
+        datePickerDialog.setOnCancelListener(this);
+        datePickerDialog.show(getFragmentManager(), "DatePickerDialog");
+
+    }
+
+    private void initDateTimeData(){
+        if(year == 0){
+            Calendar c = Calendar.getInstance();
+            year = c.get(Calendar.YEAR);
+            month = c.get(Calendar.MONTH);
+            day = c.get(Calendar.DAY_OF_MONTH);
+            hour = c.get(Calendar.HOUR_OF_DAY);
+            minute = c.get(Calendar.MINUTE);
+        }
+    }
+
+    @Override
+    public void onCancel(DialogInterface dialog) {
+        year = month = day = hour = minute = 0;
+        tv_datetime.setText("");
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        Calendar tDefault = Calendar.getInstance();
+        tDefault.set(this.year, month, day, hour, minute);
+
+        this.year = year;
+        month = monthOfYear;
+        day = dayOfMonth;
+
+        TimePickerDialog timePickerDialog = TimePickerDialog.newInstance(
+                this,
+                tDefault.get(Calendar.HOUR_OF_DAY),
+                tDefault.get(Calendar.MINUTE),
+                true
+        );
+        timePickerDialog.setOnCancelListener(this);
+        timePickerDialog.show(getFragmentManager(), "TimePickerDialog");
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute, int second) {
+        if(hourOfDay < 9 || hourOfDay > 18){
+            onDateSet(null, this.year, this.month, this.day);
+            Toast.makeText(this, "Somente entre 9h e 18h", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        this.hour = hourOfDay;
+        this.minute = minute;
+
+        tv_datetime.setText((day < 10 ? "0"+day : day)+"/"+
+                ((month + 1) < 10 ? "0"+(month + 1) : (month + 1))+"/"+
+                year +"às "+
+                (hour < 10 ? "0"+hour : hour)+"h"+
+                (minute < 10 ? "0"+minute : minute));
     }
 }
