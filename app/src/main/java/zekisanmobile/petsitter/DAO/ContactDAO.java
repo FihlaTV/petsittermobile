854b3343c5a07b21931246f7ec41816afe43e50a
Ezequiel <zeki-san@hotmail.com>
2016-03-01 23:16:37 -0300

    Methods to find and create contacts.

--- app/src/main/java/zekisanmobile/petsitter/DAO/ContactDAO.java
+++ app/src/main/java/zekisanmobile/petsitter/DAO/ContactDAO.java
@@ -1,8 +1,12 @@
 package zekisanmobile.petsitter.DAO;
 
+import java.util.Date;
+
 import io.realm.Realm;
 import io.realm.RealmResults;
 import zekisanmobile.petsitter.Model.Contact;
+import zekisanmobile.petsitter.Model.Owner;
+import zekisanmobile.petsitter.Model.Sitter;
 
 public class ContactDAO {
 
@@ -11,4 +15,32 @@ public class ContactDAO {
         return realm.where(Contact.class).findAll();
     }
 
+    public static Contact findContact(long id){
+        Realm realm = Realm.getDefaultInstance();
+        return realm.where(Contact.class).equalTo("id", id).findFirst();
+    }
+
+    public static Contact insertOrUpdateSitter(long id, Date date_start, Date date_final, String time_start,
+                                               String time_final, Sitter sitter, Owner owner){
+        Realm realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+        Contact newContact;
+
+        if((newContact = findContact(id)) == null) {
+            newContact = realm.createObject(Contact.class);
+            newContact.setId(getAllContacts().size() + 1);
+        }
+
+        newContact.setId(id);
+        newContact.setDate_start(date_start);
+        newContact.setDate_final(date_final);
+        newContact.setTime_start(time_start);
+        newContact.setTime_final(time_final);
+        newContact.setSitter(sitter);
+        newContact.setOwner(owner);
+
+        realm.commitTransaction();
+
+        return newContact;
+    }
 }
--- app/src/main/java/zekisanmobile/petsitter/Handlers/GetContactsHandler.java
+++ app/src/main/java/zekisanmobile/petsitter/Handlers/GetContactsHandler.java
@@ -1,35 +1,64 @@
 package zekisanmobile.petsitter.Handlers;
 
+import android.content.Context;
 import android.os.AsyncTask;
 
-import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
-public class GetContactsHandler extends AsyncTask<String, Void, Void>{
+import zekisanmobile.petsitter.Model.Contact;
 
-    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+public class GetContactsHandler extends AsyncTask<String, Void, ArrayList<Contact>>{
+
     private final static String BASE_SEARCH_URL = "https://petsitterapi.herokuapp.com/api/v1/sitters/";
     private final static String FINAL_SEARCH_URL = "/contacts";
     private OkHttpClient client = new OkHttpClient();
 
+    private ArrayList<Contact> returnedContacts = new ArrayList<Contact>();
+
+    private Context context;
+
+    public GetContactsHandler(Context context){
+        this.context = context;
+    }
+
     @Override
-    protected Void doInBackground(String... params) {
-        RequestBody body = RequestBody.create(JSON, params[0]);
+    protected ArrayList<Contact> doInBackground(String... params) {
         Request request = new Request.Builder()
                 .url(BASE_SEARCH_URL + params[1] + FINAL_SEARCH_URL)
-                .post(body)
                 .build();
 
         try {
-            client.newCall(request).execute();
+            Response response = client.newCall(request).execute();
+            String jsonData = response.body().string();
+            JSONArray jsonArray = new JSONArray(jsonData);
+
+            for(int i = 0; i < jsonArray.length(); i++){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                // TODO: localizar ou criar o sitter do contact
+                // TODO: localizar ou criar o owner do contact
+                // TODO: localizar ou criar o contact
+            }
         }catch (IOException e){
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
         return null;
+    }
+
+    @Override
+    protected void onPostExecute(ArrayList<Contact> receivedContacts) {
+        // TODO: passar os contacts para a Activity
     }
 }
