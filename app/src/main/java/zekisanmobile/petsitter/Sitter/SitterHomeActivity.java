356ab8542c7e7a25bbc731bc9e21ef50738f2b1f
Ezequiel <zeki-san@hotmail.com>
2016-03-13 18:36:30 -0300

    Dependencies upgrade.

--- app/src/main/java/zekisanmobile/petsitter/Sitter/SitterHomeActivity.java
+++ app/src/main/java/zekisanmobile/petsitter/Sitter/SitterHomeActivity.java
@@ -3,6 +3,7 @@ package zekisanmobile.petsitter.Sitter;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -32,7 +33,7 @@ public class SitterHomeActivity extends AppCompatActivity
 
     @Bind(R.id.drawer_layout_sitter_home) DrawerLayout drawer;
     @Bind(R.id.toolbar_sitter_home) Toolbar toolbar;
-    @Bind(R.id.nav_view) NavigationView navigationView;
+    @Bind(R.id.nav_view_sitter_home) NavigationView navigationView;
     @Bind(R.id.rv_list_received_contacts) RecyclerView recyclerView;
     private User user;
     private ContactListAdapter adapter;
@@ -48,13 +49,16 @@ public class SitterHomeActivity extends AppCompatActivity
         user = UserDAO.getLoggedUser(1);
         contacts = new ArrayList<Contact>();
 
+        configureToolbar();
+        configureNavigationDrawer();
+        configureNavigationView();
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        );
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+        configureAdapter();
+        configureRecyclerView();
+        new GetContactsHandler(this).execute(String.valueOf(user.getSitter().getApiId()));
+    }
 
+    private void configureNavigationView() {
         navigationView.setNavigationItemSelectedListener(this);
         View header = navigationView.getHeaderView(0);
         Context context = getApplicationContext();
@@ -67,11 +71,13 @@ public class SitterHomeActivity extends AppCompatActivity
 
         TextView tvUserEmail = (TextView) header.findViewById(R.id.tvUserEmail);
         tvUserEmail.setText(user.getEmail());
+    }
 
-        configureToolbar();
-        configureAdapter();
-        configureRecyclerView();
-        new GetContactsHandler(this).execute(String.valueOf(user.getSitter().getApiId()));
+    private void configureNavigationDrawer() {
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close        );
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
     }
 
     private void configureRecyclerView() {
@@ -105,6 +111,7 @@ public class SitterHomeActivity extends AppCompatActivity
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        return false;
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
