b1cd18c71ab1c44a4b06a80334e163aa671b07a5
Ezequiel <zeki-san@hotmail.com>
2016-04-04 21:09:02 -0300

    Updated sitters list layout.

--- app/src/main/java/zekisanmobile/petsitter/Adapters/SitterAdapter.java
+++ app/src/main/java/zekisanmobile/petsitter/Adapters/SitterAdapter.java
@@ -27,15 +27,21 @@ public class SitterAdapter extends RecyclerView.Adapter<SitterAdapter.ViewHolder
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         public ImageView iv_sitter;
+        public ImageView iv_positive;
+        public ImageView iv_negative;
         public TextView tv_name;
-        public TextView tv_descricao;
+        public TextView tv_like_percent;
+        public TextView tv_dislike_percent;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
             iv_sitter = (ImageView) itemView.findViewById(R.id.iv_sitter);
+            iv_positive = (ImageView) itemView.findViewById(R.id.iv_positive);
+            iv_negative = (ImageView) itemView.findViewById(R.id.iv_negative);
             tv_name = (TextView) itemView.findViewById(R.id.tv_name);
-            tv_descricao = (TextView) itemView.findViewById(R.id.tv_descricao);
+            tv_like_percent = (TextView) itemView.findViewById(R.id.tv_like_percent);
+            tv_dislike_percent = (TextView) itemView.findViewById(R.id.tv_dislike_percent);
 
             itemView.setOnClickListener(this);
         }
@@ -66,7 +72,8 @@ public class SitterAdapter extends RecyclerView.Adapter<SitterAdapter.ViewHolder
         int imageId = parentContext.getResources().getIdentifier(sitter.getPhoto(), "drawable", parentContext.getPackageName());
         holder.iv_sitter.setImageResource(imageId);
         holder.tv_name.setText(sitter.getName());
-        holder.tv_descricao.setText("Cuidador de animais");
+        holder.tv_like_percent.setText("95%");
+        holder.tv_dislike_percent.setText("5%");
     }
 
     @Override
