b63d9f4d20667cbe8ca115e85f483d4f0db1fbf1
Ezequiel <zeki-san@hotmail.com>
2016-04-11 23:33:38 -0300

    Started evaluation.

---
+++ app/src/main/java/zekisanmobile/petsitter/Owner/EvaluationActivity.java
@@ -0,0 +1,15 @@
+package zekisanmobile.petsitter.Owner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import zekisanmobile.petsitter.R;
+
+public class EvaluationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evaluation);
+    }
+}
